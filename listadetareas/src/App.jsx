327440f7f93ec0d006602897685224d4e9c7078b
IsabellaMuroni <isa_sk_@hotmail.com>
2023-11-21 19:20:43 -0300

    Funcionando

--- listadetareas/src/App.jsx
+++ listadetareas/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TaskListContainer from './components/TaskListContainer/TaskListContainer'
+import TaskContainer from './components/TaskContainer/TaskContainer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <TaskContainer/>
     </>
   )
 }
