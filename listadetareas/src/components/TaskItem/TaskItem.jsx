a7efd55faf0ac8f048e74456fc7077fb42366bdb
DarioCeballoss <dario_tec_aut@hotmail.com.ar>
2023-11-21 19:05:08 -0300

    dario

--- listadetareas/src/components/TaskItem/TaskItem.jsx
+++ listadetareas/src/components/TaskItem/TaskItem.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const TaskItem = () => {
     return (
         <div>
-            <h1>Task Item</h1>
+            
+            <h1>Task Item2</h1>
         </div>
     );
 }
 
 export default TaskItem;
+//git
