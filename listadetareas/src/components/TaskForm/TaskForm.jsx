4f64f854141b504ca65a81d321966d466fee2c35
IsabellaMuroni <isa_sk_@hotmail.com>
2023-11-22 19:07:50 -0300

    Se agregaron funciones

--- listadetareas/src/components/TaskForm/TaskForm.jsx
+++ listadetareas/src/components/TaskForm/TaskForm.jsx
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import TaskItem from '../TaskItem/TaskItem';
 
 const TaskForm = () => {
+    const [tarea, setTarea] = useState('');
+    const [descripcion, setDescripcion] = useState();
     return (
         <>
             <section className='container-fluid bg-light-subtle rounded border border-secondary'>
                 <main className='row'>
                     <h3>Nueva Tarea</h3>
-                    <form className='row align-items-center' >
+                    <form className='row' onSubmit = {handleSubmit}>
                     
                         <div className="col-4">
                             <label class="visually-hidden" for="tarea">Tarea</label>
-                            <input type="text" className="form-control" id="tarea" placeholder='Tarea'/>
+                            <input type="text" value = {tarea} className="form-control" id="tarea" placeholder='Tarea'/>
                         </div>
     
                         <div className="col-md-6">
                             <label class="visually-hidden" for="descripcion">Descripción</label>
-                            <input type="text" className="form-control" id="descripcion" placeholder='Descripción'/>
+                            <input type="text" value = {descripcion} className="form-control" id="descripcion" placeholder='Descripción'/>
                         </div>
                         <div className="col-md-2">    
                             <button type="submit" className='btn btn-warning' ><i class="bi bi-plus-circle-fill"></i></button>
