107beeccc957e23ab7edebaa6d05011904f3bcdc
IsabellaMuroni <isa_sk_@hotmail.com>
2023-11-21 21:44:32 -0300

    Formulario modificado

--- listadetareas/src/components/TaskForm/TaskForm.jsx
+++ listadetareas/src/components/TaskForm/TaskForm.jsx
@@ -7,15 +7,16 @@ const TaskForm = () => {
             <section className='container-fluid bg-light-subtle rounded border border-secondary'>
                 <main className='row'>
                     <h3>Nueva Tarea</h3>
-                    <form className='row' >
+                    <form className='row align-items-center' >
                     
-                        <div className="col-md-4">
-                            <label for="tarea" className="form-label">Tarea</label>
-                            <input type="text" className="form-control" id="tarea"/>
+                        <div className="col-4">
+                            <label class="visually-hidden" for="tarea">Tarea</label>
+                            <input type="text" className="form-control" id="tarea" placeholder='Tarea'/>
                         </div>
+    
                         <div className="col-md-6">
-                            <label for="descripcion" className="form-label">Descripción</label>
-                            <input type="text" className="form-control" id="descripcion"/>
+                            <label class="visually-hidden" for="descripcion">Descripción</label>
+                            <input type="text" className="form-control" id="descripcion" placeholder='Descripción'/>
                         </div>
                         <div className="col-md-2">    
                             <button type="submit" className='btn btn-warning' ><i class="bi bi-plus-circle-fill"></i></button>
